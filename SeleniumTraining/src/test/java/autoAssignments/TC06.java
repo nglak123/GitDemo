fbe9e472c7b4ae7ca16516935f48dcf6eed3171f
nglak123 <lakshmin3135@gmail.com>
2023-02-14 11:33:40 -0800

    TC6

--- SeleniumTraining/src/test/java/autoAssignments/TC06.java
+++ SeleniumTraining/src/test/java/autoAssignments/TC06.java
@@ -4,11 +4,11 @@ import java.io.IOException;
 
 import org.openqa.selenium.By;
 
-import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 import org.openqa.selenium.interactions.Actions;
-import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 public class TC06 extends TC05 {
 	
